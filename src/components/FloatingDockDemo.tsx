84eec8c75f1e3ecdd674e529d2fc1d46814fd08c
bathiya pathum <it21180552@my.sliit.lk>
2025-06-12 00:12:31 +0530

    init

---
+++ src/components/FloatingDockDemo.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandFacebook,
+  IconPhone,
+  IconSchool,
+} from "@tabler/icons-react";
+
+export function FloatingDockDemo() {
+  const links = [
+    {
+      title: "LinkedIn",
+      icon: (
+        <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "//www.linkedin.com/in/thilina-hansana",
+    },
+
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://github.com/thilinahansana",
+    },
+    {
+      title: "Instagram",
+      icon: (
+        <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://www.instagram.com/hansana1100/profilecard/?igsh=cnJ1bG8zNmt0ZnYw",
+    },
+    {
+      title: "Education",
+      icon: (
+        <IconSchool className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "4",
+    },
+    {
+      title: "facebook",
+      icon: (
+        <IconBrandFacebook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://www.facebook.com/share/ouLQ19LXrSCFz3fG/?mibextid=LQQJ4d",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "6",
+    },
+    {
+      title: "Contact",
+      icon: (
+        <IconPhone className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#Contact",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-center  w-full">
+      <FloatingDock
+        mobileClassName="translate-y-20" // only for demo, remove for production
+        items={links}
+      />
+    </div>
+  );
+}
